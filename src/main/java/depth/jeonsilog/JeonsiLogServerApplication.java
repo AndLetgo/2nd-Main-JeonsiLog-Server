3ecf655cc0b8661d369c37381e776e56b12589b7
phonil <phonil@naver.com>
2023-11-26 22:25:33 +0900

    setting: CICD

---
+++ src/main/java/depth/jeonsilog/JeonsiLogServerApplication.java
@@ -0,0 +1,13 @@
+package depth.jeonsilog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeonsiLogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeonsiLogServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/depth/jeonsilog/JeonsiLogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package depth.jeonsilog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeonsiLogServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
