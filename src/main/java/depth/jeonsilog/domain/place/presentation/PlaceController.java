a15d97fa0dfb0d78c604dfc09657d601eefa8c9a
phonil <phonil@naver.com>
2023-12-24 17:11:18 +0900

    [FEAT]: Place API Swagger 설정 추가

--- src/main/java/depth/jeonsilog/domain/place/presentation/PlaceController.java
+++ src/main/java/depth/jeonsilog/domain/place/presentation/PlaceController.java
@@ -1,7 +1,14 @@
 package depth.jeonsilog.domain.place.presentation;
 
+import depth.jeonsilog.domain.exhibition.dto.ExhibitionResponseDto;
 import depth.jeonsilog.domain.place.application.PlaceService;
+import depth.jeonsilog.global.payload.ErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +22,13 @@ public class PlaceController {
 
     private final PlaceService placeService;
 
-
     // Description : 전시 공간의 전시회 목록 조회
     // TODO : 논의 후 페이징 처리
+    @Operation(summary = "전시 공간의 전시회 목록 조회", description = "전시 공간의 전시회 목록을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회 성공", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ExhibitionResponseDto.ExhibitionInPlaceRes.class))}),
+            @ApiResponse(responseCode = "400", description = "조회 실패", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+    })
     @GetMapping("/exhibition/{placeId}")
     public ResponseEntity<?> findExhibitionListInPlace(
             @Parameter(description = "전시 공간에서 진행되는 전시회 목록을 페이지별로 조회합니다. **Page는 0부터 시작합니다!**", required = true) @RequestParam(value = "page") Integer page,
