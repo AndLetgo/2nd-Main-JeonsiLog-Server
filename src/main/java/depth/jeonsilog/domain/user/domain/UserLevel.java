3fe788e59b76223e77d358a4579dda621e8d43af
강승우 <phonil@naver.com>
2024-04-25 03:10:36 +0900

    [CHORE]: UserLever EXIST -> DONE 변경

--- src/main/java/depth/jeonsilog/domain/user/domain/UserLevel.java
+++ src/main/java/depth/jeonsilog/domain/user/domain/UserLevel.java
@@ -5,7 +5,7 @@ import org.springframework.expression.spel.ast.OpNE;
 public enum UserLevel {
 
     NON, // 레벨 없음 : 0개
-    EXSIST, // 첫 감상평 작성 이후 (여전히 레벨 없음 단계) : 1 ~ 2개
+    DONE, // 첫 감상평 작성 이후 (여전히 레벨 없음 단계) : 1 ~ 2개
     BEGINNER, // 초보 : 3 ~ 9개
     INTERMEDIATE, // 중수 : 10 ~ 19개
     ADVANCED, // 고수 : 20 ~ 29개
