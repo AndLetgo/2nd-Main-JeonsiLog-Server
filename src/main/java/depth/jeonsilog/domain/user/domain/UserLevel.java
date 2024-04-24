72a380e7c7b14b5882e560f66f3b9f17a85d584b
강승우 <phonil@naver.com>
2024-04-25 03:00:08 +0900

    [REFACTOR]: UserLevel 추가

---
+++ src/main/java/depth/jeonsilog/domain/user/domain/UserLevel.java
@@ -0,0 +1,13 @@
+package depth.jeonsilog.domain.user.domain;
+
+import org.springframework.expression.spel.ast.OpNE;
+
+public enum UserLevel {
+
+    NON, // 레벨 없음 : 0개
+    EXSIST, // 첫 감상평 작성 이후 (여전히 레벨 없음 단계) : 1 ~ 2개
+    BEGINNER, // 초보 : 3 ~ 9개
+    INTERMEDIATE, // 중수 : 10 ~ 19개
+    ADVANCED, // 고수 : 20 ~ 29개
+    MASTER // 마스터 : 30개 ~
+}
